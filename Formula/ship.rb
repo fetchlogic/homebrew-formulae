c8b66b8105a3730c1f2d96260c25159861508b7a
Josh Sherman <josh@gravityblvd.com>
2016-07-25 21:52:21 -0500

    Fixed hash
    No fucking clue why this happens. Had it happen on some other projects
    too

--- Formula/ship.rb
+++ Formula/ship.rb
@@ -3,7 +3,7 @@ require "formula"
 class Ship < Formula
   homepage "https://github.com/fetchlogic/ship"
   url "https://github.com/fetchlogic/ship/archive/1.0.0.zip"
-  sha1 "5e374311ceb2f2638cb948f8fc10204ba1304ef6"
+  sha1 "e6d20aa0c9f4b380bf8f3df48a79940bb2944a8e"
 
   def install
     bin.install 'ship'
